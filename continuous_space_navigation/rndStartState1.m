8afc3af64096208efa36e8e7467e508de170b91a
SharathRaparthy <sharathchandra.raparthy@gmail.com>
2018-11-15 20:07:08 +0530

    rndStartState added

---
+++ continuous_space_navigation/rndStartState1.m
@@ -0,0 +1,26 @@
+function s = rndStartState1()
+
+global pos_x
+global pos_y
+global pos_z
+
+s = 0;
+random_x=pos_x;
+random_y=pos_y;
+while (((random_x^2)+(random_y^2))<((0.075)^2))
+     fprintf('Goal Reached\n');
+     s = 1;
+     random_angle = (2*rand(1,1))*pi; 
+     random_x=-0.10+0.2*rand(1,1);
+     random_y=-0.10+0.2*rand(1,1);
+         
+end
+if s==1
+      gazebo = ExampleHelperGazeboCommunicator();
+     models = getSpawnedModels(gazebo);
+     epuck = ExampleHelperGazeboSpawnedModel('epuck2',gazebo);
+     setState(epuck,'position',[random_x random_y pos_z],'orientation',[0 0 random_angle]);
+     pause(0.1);
+end
+
+end
